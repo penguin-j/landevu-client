1827830a12cee5f97f6a23776a6c2878d2dc1409
j <penguinjinbei@gmail.com>
2023-06-16 01:36:28 +0900

    feat: add area recommend form component

--- src/pages/areas/recommend.tsx
+++ src/pages/areas/recommend.tsx
@@ -1,3 +1,27 @@
+import { useState } from 'react'
+
 export default function RecommendArea() {
-  return <h1>Recommendation</h1>
+  const RecommendAreaForm = () => {
+    const [name, setName] = useState('')
+    const handleNameChange = (event) => {
+      setName(event.target.value)
+    }
+    const handleSubmit = () => {
+      // todo
+    }
+
+    return (
+      <div>
+        <input type='text' value={name} onChange={handleNameChange} />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <h1>Recommendation</h1>
+      <RecommendAreaForm />
+    </>
+  )
 }
